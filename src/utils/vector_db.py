473a1e9e9ca2ae14f5866de6e93861157a51dddc
Siddharth Yadav <siddharth.yadav@hilabs.com>
2025-06-24 14:04:31 +0530

    persisting vector db

--- src/utils/vector_db.py
+++ src/utils/vector_db.py
@@ -9,12 +9,18 @@ from typing import List, Union
 
 class VectorDB:
     def __init__(
-        self, id_key: str = "doc_id", collection_name: str = "multi_modal_rag"
+        self,
+        id_key: str = "doc_id",
+        collection_name: str = "multi_modal_rag",
+        persist_directory: str = "./chroma_db",
     ):
         self.id_key = id_key
+        self.persist_directory = persist_directory
         self.store = InMemoryStore()
         self.vector_store = Chroma(
-            collection_name=collection_name, embedding_function=OpenAIEmbeddings()
+            collection_name=collection_name,
+            persist_directory=persist_directory,
+            embedding_function=OpenAIEmbeddings(),
         )
         self.retriever = MultiVectorRetriever(
             vectorstore=self.vector_store,
@@ -22,12 +28,15 @@ class VectorDB:
             id_key=self.id_key,
         )
 
+    def persist(self):
+        self.vector_store.persist()
+
     def add_documents(
         self,
         summaries: List[str],
         originals: List[Union[str, object]],
         file_name: str,
-        element_type: str
+        element_type: str,
     ):
         doc_ids = [str(uuid.uuid4()) for _ in summaries]
         summary_docs = [
@@ -36,7 +45,7 @@ class VectorDB:
                 metadata={
                     self.id_key: doc_ids[i],
                     "file_name": file_name,
-                    "element_type": element_type
+                    "element_type": element_type,
                 },
             )
             for i in range(len(summaries))
@@ -44,9 +53,9 @@ class VectorDB:
 
         self.vector_store.add_documents(summary_docs)
         self.store.mset(list(zip(doc_ids, originals)))
-    
+
     def query(self, query: str, k: int = 8):
         return self.retriever.invoke(query)
-    
+
     def get_retriever(self):
         return self.retriever
